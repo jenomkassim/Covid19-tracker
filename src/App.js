3ce159b08a5855a0926bab22732ecd19319c941f
Jenom Kassim <jenomkassim@gmail.com>
2020-09-12 03:21:46 +0100

    Created Top Row and made API call to get all country names and ISO from https://disease.sh/v3/covid-19/countries

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+import {MenuItem, FormControl, Select} from "@material-ui/core"
 import './App.css';
 
 function App() {
+    const [countries, setCountries] = useState([])
+
+    useEffect(() => {
+        const getCountriesData = async () => {
+            await fetch("https://disease.sh/v3/covid-19/countries")
+                .then((response) => response.json())
+                .then((data) => {
+                    const countries = data.map((country) => ({
+                        name: country.country, // NIGERIA
+                        value: country.countryInfo.iso2 // NG
+                    }))
+
+                    setCountries(countries)
+                })
+        }
+
+        getCountriesData()
+    }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+        <div className="app__header">
+            <h1>Building a Covid 19 tracker</h1>
+
+            <FormControl className="app__dropdown">
+                <Select variant="outlined" value="abc">
+                    {countries.map((country) =>(
+                        <MenuItem value={country.value}>{country.name}</MenuItem>
+                    ))}
+
+                    {/*<MenuItem value="worldwide">WorldWide</MenuItem>*/}
+                    {/*<MenuItem value="local">Local</MenuItem>*/}
+                </Select>
+            </FormControl>
+        </div>
     </div>
   );
 }
