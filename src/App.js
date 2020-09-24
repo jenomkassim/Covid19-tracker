ffdb90dfe7ec75ba10ba084385905808cb17b4d6
Jenom Kassim <jenomkassim@gmail.com>
2020-09-24 01:07:26 +0100

    Started interaction with the "https://disease.sh" api to achieve the following: - Create a dropdown list with all countries - Dynamically change the stats based off what country is currently selected - Get all countries and their current coronavirus cases

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React, {useEffect, useState} from 'react';
-import {MenuItem, FormControl, Select} from "@material-ui/core"
-import './App.css';
+import {MenuItem, FormControl, Select, CardContent, Card, Typography} from "@material-ui/core"
+import './App.css'
+import InfoBox from './InfoBox'
+import Map from "./Map";
+import Table from "./Table";
 
 function App() {
     const [countries, setCountries] = useState([])
+    const [country, setCountry] = useState('worldwide')
+    const [countryInfo, setCountryInfo] = useState({})
+    const [tableData, setTableData] = useState([])
+
+    useEffect(() => {
+        fetch("https://disease.sh/v3/covid-19/all")
+            .then(response => response.json())
+            .then(data => {
+                setCountryInfo(data)
+            })
+    }, [])
 
     useEffect(() => {
         const getCountriesData = async () => {
@@ -15,6 +29,7 @@ function App() {
                         value: country.countryInfo.iso2 // NG
                     }))
 
+                    setTableData(data)
                     setCountries(countries)
                 })
         }
@@ -22,22 +37,56 @@ function App() {
         getCountriesData()
     }, [])
 
+    const onCountryChange = async (event) => {
+        const countryCode = event.target.value
+
+        const url = countryCode === "worldwide" ? 'https://disease.sh/v3/covid-19/all'
+            : `https://disease.sh/v3/covid-19/countries/${countryCode}`
+
+        await fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                setCountry(countryCode)
+                setCountryInfo(data)
+            })
+    }
+
   return (
     <div className="app">
-        <div className="app__header">
-            <h1>Building a Covid 19 tracker</h1>
+        <div className="app__left">
+            <div className="app__header">
+                <h1>Covid 19 tracker</h1>
 
-            <FormControl className="app__dropdown">
-                <Select variant="outlined" value="abc">
-                    {countries.map((country) =>(
-                        <MenuItem value={country.value}>{country.name}</MenuItem>
-                    ))}
+                <FormControl className="app__dropdown">
+                    <Select variant="outlined" value={country} onChange={onCountryChange}>
+                        <MenuItem value="worldwide">Worldwide</MenuItem>
 
-                    {/*<MenuItem value="worldwide">WorldWide</MenuItem>*/}
-                    {/*<MenuItem value="local">Local</MenuItem>*/}
-                </Select>
-            </FormControl>
+                        {countries.map((country) =>(
+                            <MenuItem value={country.value}>{country.name}</MenuItem>
+                        ))}
+
+                    </Select>
+                </FormControl>
+            </div>
+
+            <div className="app__stats">
+                <InfoBox title="Coronavirus Cases" cases={countryInfo.todayCases} total={countryInfo.cases}/>
+                <InfoBox title="Recovered" cases={countryInfo.todayRecovered} total={countryInfo.recovered} />
+                <InfoBox title="Deaths" cases={countryInfo.todayDeaths} total={countryInfo.deaths} />
+            </div>
+
+            <div className="app__map">
+                <Map />
+            </div>
         </div>
+
+        <Card className="app__right">
+            <CardContent>
+                <Typography>Live Cases By Countries</Typography>
+                <Table countries={tableData} />
+            </CardContent>
+        </Card>
+
     </div>
   );
 }
---
+++ src/Map.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Map() {
+   return(
+       <h1>
+           This is a map
+       </h1>
+   )
+}
+
+export default Map
